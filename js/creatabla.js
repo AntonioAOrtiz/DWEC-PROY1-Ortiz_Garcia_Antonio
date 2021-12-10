9c5445f681f6696e3fe4e2d5659e7bde3904fa89
AntonioAOrtiz <aaortiz.alumno@iesrafaelalberti.es>
2021-12-10 21:40:19 +0100

    Funcion para crear tabla, el boton desaparece

--- js/creatabla.js
+++ js/creatabla.js
@@ -1,10 +1,11 @@
-let datosTabla = [[1, " Lebron James",38+" MinutosPP"],[2," Kevin Durant",37.2+" MinutosPP"],[3," James Harden",37.1+" MinutosPP"],[2," Luka Doncic",36.2+" MinutosPP"],[5," Devin Booker",36+" MinutosPP"]];
+let datosTabla = [[1, " Lebron James",38+" MinutosPP"],[2," Kevin Durant",37.2+" MinutosPP"],[3," James Harden",37.1+" MinutosPP"],[4," Luka Doncic",36.2+" MinutosPP"],[5," Devin Booker",36+" MinutosPP"]];
 
 function crearTabla(datosTabla) {
     let tabla = document.createElement('table');
     let cuerpoTabla = document.createElement('tbody');
     let section = document.getElementById("contTabla")
     let boton = document.getElementById("botonMinuto");
+    
 
     datosTabla.forEach(function(datosFilas) {
         let fila = document.createElement('tr');
@@ -23,4 +24,6 @@ function crearTabla(datosTabla) {
     section.appendChild(tabla);
     
     section.insertBefore(tabla,boton);
+
+    boton.style.display= "none";
 }
